3cf759733dd9fa9b6ceab8db1c2685618dc73a03
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-10-20 18:17:25 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,46 @@
 class PigLatinizer
 
-def piglatinize(word)
-    return word if %w[and an in].include?(word)
-    letters = word.split("")
-    letters.keep_if {|letter| letter != "."}
-    if letters.size > 1
-      until vowel?(letters[0])
-        letters << letters.shift
+  def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  end
+
+  private
+
+
+  def vowel?(char)
+    char.match?(/[AaEeIiOoUu]/)
+  end
+
+
+  def pig_latinized_const_count(word)
+    letters = []
+    word.split("").each do |char|
+      if !vowel?(char)
+       letters.push(char)
+      else
+        break
       end
-      letters  << "ay"
     end
-    letters.join
+    letters.size.to_i
   end
 
-  def to_pig_latin(text)
-    words = text.split(" ")
-    words.map! {|word| piglatinize(word)}
-    words.join(" ")
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
   end
 
-  def vowel?(letter)
-    letter.downcase
-    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
+
+  def piglatinize_word(word)
+    sliced_string = word.slice(pig_latinized_const_count(word), word.length - 1)
+    const_string = word.slice(0, pig_latinized_const_count(word))
+    pig_latin_word = ""
+
+    if vowel?(word[0])
+      pig_latin_word = word + "way"
+    else
+      pig_latin_word = sliced_string + const_string + "ay"
+    end
+    pig_latin_word
   end
 
 end

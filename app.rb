fef3d8f205ea71d14cc0b9e8b231f286f2c66412
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-10-20 18:20:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    to_piglatinize = PigLatinizer.new
+    @piglatin = to_piglatinize.piglatinize(params[:user_phrase])
+
+    erb :results
+  end
 end

3b9b8a475fb5c9b2ee4130770a9f3a3cf578e3ea
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-10-20 18:00:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,10 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
+
+  post '/piglatinize' do
+    @text = params[:user_phrase]
+    @piglatinizer = PigLatinizer.new
+    erb :piglatinize
+  end
 end
